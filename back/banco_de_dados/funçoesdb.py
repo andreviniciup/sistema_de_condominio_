aa3975de95e5883e8c5a6710f1ff848cc9d4452c
andreviniciup <167660879+andreviniciup@users.noreply.github.com>
2024-06-06 09:47:17 -0300

    nova atualização

--- back/banco_de_dados/funçoesdb.py
+++ back/banco_de_dados/funçoesdb.py
@@ -1,24 +1,4 @@
 import sqlite3
-import re
-
-def validar_nome(nome):
-    match = re.match(r'^[a-zA-Z\s]{1,50}$', nome)
-    return match is not None
-
-# Função para validar CPF
-def validar_cpf(cpf):
-    match = re.match(r'^\d{11}$', cpf) 
-    return match is not None
-
-# Função para validar telefone
-def validar_telefone(telefone):
-    match = re.match(r'^\d{11}$', telefone)
-    return match is not None
-
-def validar_data_nascimento(data_nascimento):
-    match = re.match(r'^\d{8}$', data_nascimento)
-    return match is not None
-
 
 def inserir_morador(nome, cpf, data_nascimento, telefone, bloco, apartamento, placa_carro):
     conn = sqlite3.connect('condominio.db')
--- back/banco_de_dados/validar_entry.py
+++ back/banco_de_dados/validar_entry.py
@@ -18,14 +18,26 @@ def validar_data_nascimento(data_nascimento):
     match = re.match(r'^\d{8}$', data_nascimento)
     return match is not None
 
-def validar_data_nascimento(data_nascimento):
-    match = re.match(r'^\d{8}$', data_nascimento)
-    return match is not None
-
 def validar_bloco(bloco):
     match = re.match(r'^[a-zA-Z\s]{1}$', bloco)
     return match is not None
 
 def validar_apartamento(apartamento):
     match = re.match(r'^\d{3}$', apartamento)
+    return match is not None
+
+def placa_carro(placa_carro):
+    match = re.match(r'^[a-zA-Z0-9\s]{1,50}$', placa_carro)
+    return match is not None
+    
+def validar_porteiro(porteiro):
+    match = re.match(r'^[a-zA-Z\s]{1,50}$', porteiro)
+    return match is not None
+
+def validar_data_entrega(data_entrega):
+    match = re.match(r'^\d{8}$', data_entrega)
+    return match is not None
+
+def validar_data_retirada(data_retirada):
+    match = re.match(r'^\d{8}$', data_retirada)
     return match is not None
